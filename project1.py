380cf0cf0588102f334eecb90eeea18cf6bd509c
buttiharshitha8-oss <buttiharshitha8@gmail.com>
2026-07-07 08:18:39 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,22 @@
+tasks = []
+while True:
+    print("\n1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task Added Successfully!")
+        elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            print("\nYour Tasks:")
+            for i, task in enumerate(tasks, start=1):
+                print(i, ".", task)
+    elif choice == "3":
+        print("Thank you!")
+        break
+    else:
+        print("Invalid Choice")
